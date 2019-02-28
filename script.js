8827cb4da6200dfadceaaaa19de9f87c39ecdcce
Arkady Sharapov <sharapov@pne.io>
2019-02-28 20:43:42 +0300

    33

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 var validate = function(target) {
     if (target > 0) {
+        var validate = function(target) {
+            if (target > 0) {
+                return "fail";
+            } else {
+                return "success";
+            }
+        
+        
+        };
         return "fail";
     } else {
+        var validate = function(target) {
+            if (target > 0) {
+                return "fail";
+            } else {
+                return "success";
+            }
+        
+        
+        };
         return "success";
     }
 
