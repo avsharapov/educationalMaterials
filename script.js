1877db8d0cf114e9e83217c1097d5cfbec621eb5
Arkady Sharapov <sharapov@pne.io>
2019-02-28 20:49:16 +0300

    pqoiwuerier

--- script.js
+++ script.js
@@ -7,9 +7,24 @@ var validate = function(target) {
                 return "success";
             }
         
-        
+            if (target > 0) {
+                return "fail";
+            } else {
+                return "success";
+            }
         };
-
+        if (target > 0) {
+            return "fail";
+        } else {
+            return "success";
+        }
+        return "fail";
+        if (target > 0) {
+            return "fail";
+        } else {
+            return "success";
+        }
+    } else {
         var validate = function(target) {
             if (target > 0) {
                 return "fail";
@@ -19,28 +34,22 @@ var validate = function(target) {
         
         
         };
-        return "fail";
-        
-    var validate = function(target) {
+
         if (target > 0) {
             return "fail";
         } else {
-            clearTimeout();
+            return "success";
+        }        return "success";
+        if (target > 0) {
+            return "fail";
+        } else {
+            return "success";
         }
-    
-    
-    };
-    } else {
-
     }
 
-    var validate = function(target) {
-        if (target > 0) {
-            return "fail";
-        } else {
-            clearTimeout();
-        }
-    
-    
-    };
+    if (target > 0) {
+        return "fail";
+    } else {
+        return "success";
+    }
 };
